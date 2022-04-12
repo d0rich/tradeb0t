607281e7a853a798f9a23aedab8b17ae0a7d7483
Nikolay Dorofeev <d.orich@yandex.com>
2022-04-12 23:57:15 +0700

    Make `TradeBot` module fields private

--- bot/TradeBot.ts
+++ bot/TradeBot.ts
@@ -1,11 +1,17 @@
 import {ExchangeAnalyzer, ExchangeTrader, ExchangeWatcher} from "./modules";
 
 export class TradeBot {
-    analyzer: ExchangeAnalyzer = new ExchangeAnalyzer(this);
-    trader: ExchangeTrader = new ExchangeTrader(this);
-    watcher: ExchangeWatcher = new ExchangeWatcher(this);
+    private readonly _analyzer: ExchangeAnalyzer
+    private readonly _trader: ExchangeTrader
+    private readonly _watcher: ExchangeWatcher
 
     constructor() {
-
+        this._analyzer = new ExchangeAnalyzer(this)
+        this._trader = new ExchangeTrader(this)
+        this._watcher = new ExchangeWatcher(this)
     }
+
+    public get analyzer(): ExchangeAnalyzer { return this._analyzer }
+    public get trader(): ExchangeTrader { return this._trader }
+    public get watcher(): ExchangeWatcher { return this._watcher }
 }
