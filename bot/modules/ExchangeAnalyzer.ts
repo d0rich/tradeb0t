d873bce330f903e283e552f0e09655353b5f6ddf
Nikolay Dorofeev <dorich2000@gmail.com>
2022-04-18 00:24:27 +0700

    update portfolio

--- bot/modules/ExchangeAnalyzer.ts
+++ bot/modules/ExchangeAnalyzer.ts
@@ -3,6 +3,9 @@ import {ExchangeTrader, ExchangeWatcher} from ".";
 import {TradeAlgorithms} from "../../config/TradeAlgorithms";
 import {TradeBot} from "../TradeBot";
 
+import { PortfolioPosition, PrismaClient } from "@prisma/client";
+const db = new PrismaClient()
+
 export class ExchangeAnalyzer implements IExchangeAnalyzer{
     private _tradebot: TradeBot;
     private _tradeAlgos: TradeAlgorithms = new TradeAlgorithms(this);
@@ -21,5 +24,19 @@ export class ExchangeAnalyzer implements IExchangeAnalyzer{
 
     get tradeAlgos(): TradeAlgorithms {
         return this._tradeAlgos
+    }
+
+    async updatePortfolio(){
+        const transactionFunction = async () => {
+            db.portfolioPosition.deleteMany({})
+            const relevantPortfolio = await this.watcher.getPortfolio()
+            return relevantPortfolio.map(position => db.portfolioPosition.create({ data: position }))
+        }
+        return await db.$transaction(await transactionFunction())
+        
+    }
+
+    async getPortfolio(): Promise<PortfolioPosition[]> {
+        return db.portfolioPosition.findMany({})
     }
 }
