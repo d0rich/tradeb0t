3a28fbdce79f0fe0d44791b8f64d8d00f5945c34
Nikolay Dorofeev <dorich2000@gmail.com>
2022-04-18 17:44:29 +0700

    Change preposition for prisma types `C_` => `D_`
    D stands for Data

--- bot/modules/ExchangeAnalyzer.ts
+++ bot/modules/ExchangeAnalyzer.ts
@@ -3,7 +3,7 @@ import {ExchangeTrader, ExchangeWatcher} from ".";
 import {TradeAlgorithms} from "../../config/TradeAlgorithms";
 import {TradeBot} from "../TradeBot";
 
-import { C_Currency, C_PortfolioPosition, PrismaClient, C_Security } from "@prisma/client";
+import { D_Currency, D_PortfolioPosition, PrismaClient, D_Security } from "@prisma/client";
 const db = new PrismaClient()
 
 export class ExchangeAnalyzer implements IExchangeAnalyzer{
@@ -26,36 +26,36 @@ export class ExchangeAnalyzer implements IExchangeAnalyzer{
         return this._tradeAlgos
     }
 
-    async updatePortfolio(): Promise<C_PortfolioPosition[]>{
+    async updatePortfolio(): Promise<D_PortfolioPosition[]>{
         const transactionFunction = async () => {
-            await db.c_PortfolioPosition.deleteMany()
+            await db.d_PortfolioPosition.deleteMany()
             const relevantPortfolio = await this.watcher.getPortfolio()
-            return relevantPortfolio.map(position => db.c_PortfolioPosition.create({ data: position }))
+            return relevantPortfolio.map(position => db.d_PortfolioPosition.create({ data: position }))
         }
         return await db.$transaction(await transactionFunction())
         
     }
 
-    async getPortfolio(): Promise<C_PortfolioPosition[]> {
-        return db.c_PortfolioPosition.findMany({})
+    async getPortfolio(): Promise<D_PortfolioPosition[]> {
+        return db.d_PortfolioPosition.findMany({})
     }
 
-    async updateCurrencies(): Promise<C_Currency[]> {
+    async updateCurrencies(): Promise<D_Currency[]> {
         const transactionFunction = async () => {
             const relevantCurrencies = await this.watcher.getCurrencies()
             return relevantCurrencies
-                .map(currency => db.c_Currency.upsert({ where: {ticker: currency.ticker}, update: {}, create: currency }))
+                .map(currency => db.d_Currency.upsert({ where: {ticker: currency.ticker}, update: {}, create: currency }))
         }
         return await db.$transaction(await transactionFunction())
     }
 
-    async getCurrencies(): Promise<C_Currency[]> {
-        return await db.c_Currency.findMany({})
+    async getCurrencies(): Promise<D_Currency[]> {
+        return await db.d_Currency.findMany({})
     }
 
-    async updateSecurities(): Promise<C_Security[]> {
+    async updateSecurities(): Promise<D_Security[]> {
         const transactionFunction = async () => {
-            const portfolio: C_PortfolioPosition[] = await this.watcher.getPortfolio()
+            const portfolio: D_PortfolioPosition[] = await this.watcher.getPortfolio()
             const portfolioInfo = await Promise.all(
                 portfolio.map(async (position) => {
                     return {
@@ -65,7 +65,7 @@ export class ExchangeAnalyzer implements IExchangeAnalyzer{
                 })
             )
             const promises = portfolio
-                .map((position, index) => db.c_Security.upsert({ 
+                .map((position, index) => db.d_Security.upsert({ 
                         where: { ticker: position.security_ticker },
                         update: {
                             price: portfolioInfo[index].price,
@@ -83,7 +83,7 @@ export class ExchangeAnalyzer implements IExchangeAnalyzer{
         return await db.$transaction(await transactionFunction())
     }
 
-    async getSecurities(): Promise<C_Security[]> {
-        return await db.c_Security.findMany({})
+    async getSecurities(): Promise<D_Security[]> {
+        return await db.d_Security.findMany({})
     }
 }
