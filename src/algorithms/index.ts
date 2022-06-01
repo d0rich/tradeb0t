187bb13d48d0a6e35c9d43fbe31a468b302866f8
Nikolay Dorofeev <dorich2000@gmail.com>
2022-06-01 19:50:48 +0700

    `SlicingAlgorithms` => `SlicingAlgorithm`

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -1,7 +1,7 @@
 import {
     AbstractTradeAlgorithm
 } from "lib/modules/TradeBot";
-import {SlicingAlgorithm} from "./SlicingAlgorithms";
+import {SlicingAlgorithm} from "./SlicingAlgorithm";
 import {ExchangeAnalyzer} from "lib/modules/TradeBot";
 
 export function initAlgorithms(analyzer: ExchangeAnalyzer): AbstractTradeAlgorithm<any, any>[] {
