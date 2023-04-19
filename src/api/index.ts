6960c40235e7f29dc8237239715a4a1244af97cf
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-19 10:04:07 +0400

    Refactor ApiService initialization

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,26 @@
+import http from 'http'
+import ws from 'ws'
+import { toNodeListener } from 'h3'
+import { initH3 } from './h3'
+import { registerH3Routes, registerWSSHandler } from './trpc'
+import { ITradeBot } from 'src/bot'
+
 export * from './trpc'
-export * from './express'
 export * from './h3'
+
+export async function initH3WithWss(tradeBot: ITradeBot) {
+  const h3 = initH3(tradeBot)
+  await registerH3Routes({
+    tradeBot: tradeBot,
+    h3App: h3
+  })
+  const httpServer = http.createServer(toNodeListener(h3))
+  const wss = new ws.Server({
+    server: httpServer
+  })
+  registerWSSHandler({
+    wss: wss,
+    tradeBot: tradeBot
+  })
+  return httpServer
+}
--- src/bot/services/ApiService.ts
+++ src/bot/services/ApiService.ts
@@ -1,18 +1,15 @@
-import { App, toNodeListener } from 'h3'
 import { networkInterfaces } from 'os'
 import http from 'http'
-import ws, { WebSocketServer } from 'ws'
 import colors from 'colors/safe'
 import { ITradeBot } from '../ITradeBot'
-import { registerH3Routes, registerWSSHandler, initH3 } from 'src/api'
+import { initH3WithWss } from 'src/api'
 import { useConfig } from '../../config'
 import { HandleError } from '../../decorators'
 import { StubDomain } from 'src/domain'
 
 export class ApiService {
   private readonly tradeBot: ITradeBot<StubDomain, unknown>
-  private h3: App
-  private wss: WebSocketServer
+
   private http: http.Server
 
   constructor(tradeBot: ITradeBot) {
@@ -23,19 +20,7 @@ export class ApiService {
   @HandleError()
   private async configureServers() {
     const config = useConfig()
-    this.h3 = initH3(this.tradeBot)
-    await registerH3Routes({
-      tradeBot: this.tradeBot,
-      h3App: this.h3
-    })
-    this.http = http.createServer(toNodeListener(this.h3))
-    this.wss = new ws.Server({
-      server: this.http
-    })
-    registerWSSHandler({
-      wss: this.wss,
-      tradeBot: this.tradeBot
-    })
+    this.http = await initH3WithWss(this.tradeBot)
     this.http.listen(config.api.port, () => {
       console.info(`${colors.blue('[i]')} TradeBot is online on: `)
       if (config.api.host === '0.0.0.0') {
