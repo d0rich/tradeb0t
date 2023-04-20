57adb216bb493fb423178073db6063a880b63b30
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-20 20:17:23 +0400

    Initialize config in tradebot

---
+++ src/bot/ITradeBotConfig.ts
@@ -0,0 +1,27 @@
+export interface ITradeBotConfig {
+  auth: {
+    token: string
+    required: boolean
+  }
+  api: {
+    port: number | string
+    host: string
+  }
+  logs: {
+    directory: string
+  }
+}
+
+export const defaultConfig: ITradeBotConfig = {
+  auth: {
+    token: process.env.BOT_TOKEN || '',
+    required: true
+  },
+  api: {
+    port: 4268,
+    host: '0.0.0.0'
+  },
+  logs: {
+    directory: './logs'
+  }
+}
