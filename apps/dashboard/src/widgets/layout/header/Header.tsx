c4c905653571b44cbca13a2ae18abc64791578dc
Nikolay Dorofeev <dorich2000@gmail.com>
2023-05-23 13:43:48 +0400

    Lintfix

--- apps/dashboard/src/widgets/layout/header/Header.tsx
+++ apps/dashboard/src/widgets/layout/header/Header.tsx
@@ -4,8 +4,10 @@ export default function Header() {
   return (
     <>
       <header className="flex items-center justify-between px-4 py-3 bg-neutral-700 border-b sm:px-6">
-        <Link className='btn' href="/bots">Bots</Link>
-        <button className='btn'>United Logs</button>
+        <Link className="btn" href="/bots">
+          Bots
+        </Link>
+        <button className="btn">United Logs</button>
       </header>
     </>
   )
