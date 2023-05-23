51b18d84e395ab2359f6e2966e2a77cd90bb2a5a
Nikolay Dorofeev <dorich2000@gmail.com>
2023-05-23 13:43:20 +0400

    Fix hydration error

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,20 +1,15 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Header from '../widgets/layout/header/Header'
 import Head from 'next/head'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div lang="en">
+    <>
       <Head>
         <title>tradeb0t dashboard</title>
       </Head>
-      <body className={inter.className}>
-        <Header />
-        <main>{children}</main>
-      </body>
-    </div>
+      <Header />
+      <main>{children}</main>
+    </>
   )
 }
