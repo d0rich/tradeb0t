6168c724deaa4abe04d3808d1c87bf08f9f12a0f
Nikolay Dorofeev <dorich2000@gmail.com>
2023-05-25 12:09:08 +0400

    OrderStatus now created with enum, not just union type

--- packages/core/src/domain/models/persistent/Order.ts
+++ packages/core/src/domain/models/persistent/Order.ts
@@ -4,16 +4,25 @@ import { dateTransformer } from 'src/storage'
 import { z } from 'zod'
 import { operationType } from 'src/api/trpc/schemas'
 
-export type OrderStatus =
-  | 'not_processed'
-  | 'to_be_processed'
-  | 'placed'
-  | 'units_allocated'
-  | 'units_redeemed'
-  | 'rejected'
-  | 'cancelled'
-  | 'expired'
-  | 'undefined'
+/**
+ * @see {@link https://support.tradeplusonline.com/support/solutions/articles/1000254592-what-are-the-different-order-status-possible-of-an-order- | What are the different order status possible of an order?}
+ */
+export enum EOrderStatus {
+  NOT_PROCESSED = 'not_processed',
+  TO_BE_PROCESSED = 'to_be_processed',
+  PLACED = 'placed',
+  UNITS_ALLOCATED = 'units_allocated',
+  UNITS_REDEEMED = 'units_redeemed',
+  REJECTED = 'rejected',
+  CANCELLED = 'cancelled',
+  EXPIRED = 'expired',
+  UNDEFINED = 'undefined'
+}
+
+/**
+ * @see {@link EOrderStatus}
+ */
+export type OrderStatus = EOrderStatus | `${EOrderStatus}`
 export type OperationType = z.infer<typeof operationType>
 
 @Entity()
@@ -24,9 +33,6 @@ export class Order {
   @Column()
   securityTicker: string
 
-  /**
-   * [Statuses details]{@link https://support.tradeplusonline.com/support/solutions/articles/1000254592-what-are-the-different-order-status-possible-of-an-order-}
-   */
   @Column()
   status: OrderStatus = 'undefined'
 
