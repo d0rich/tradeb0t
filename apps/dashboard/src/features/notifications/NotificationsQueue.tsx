35ba2d996c839fd69f4e930def09967b9db9663b
Nikolay Dorofeev <dorich2000@gmail.com>
2023-05-29 11:57:39 +0400

    memo layout components

--- apps/dashboard/src/features/notifications/NotificationsQueue.tsx
+++ apps/dashboard/src/features/notifications/NotificationsQueue.tsx
@@ -1,18 +1,27 @@
+import { memo, useCallback } from 'react'
+
 import NotificationCard from '@/src/entities/notifications/ui/NotificationCard'
 import { useAppSelector, useAppDispatch } from '@/src/shared/model/hooks'
 import { removeNotification } from '@/src/entities/notifications/model/notificationsSlice'
+import { Notification } from '@/src/entities/notifications/model/Notification'
+
+const NotificationCardMemo = memo(NotificationCard)
 
 export default function NotificationsQueue() {
   const notifications = useAppSelector((state) => state.notifications.notifications)
   const dispatch = useAppDispatch()
 
+  const closeNotification = useCallback((notification: Notification) => {
+    dispatch(removeNotification(notification.createdAt))
+  }, [])
+
   return (
     <>
       {notifications.map((notification) => (
-        <NotificationCard
+        <NotificationCardMemo
           key={Number(notification.createdAt)}
           notification={notification}
-          onClose={(notification) => dispatch(removeNotification(notification.createdAt))}
+          onClose={closeNotification}
         />
       ))}
     </>
