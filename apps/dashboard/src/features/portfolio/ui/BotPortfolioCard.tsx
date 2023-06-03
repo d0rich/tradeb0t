d1d80f8b29a6be88abacd4c84fc02840393b4db2
Nikolay Dorofeev <dorich2000@gmail.com>
2023-06-03 19:47:37 +0400

    Split BotPortfolioCard into view and data

--- apps/dashboard/src/features/portfolio/ui/BotPortfolioCard.tsx
+++ apps/dashboard/src/features/portfolio/ui/BotPortfolioCard.tsx
@@ -1,6 +1,5 @@
 import { trpc } from '@/src/shared/api/trpc'
-import PortfolioListItem from '@/src/entities/portfolio/ui/SecurityBalanceListItem'
-import SecurityBalanceListItem from '@/src/entities/portfolio/ui/CurrencyBalanceView'
+import BotPortfolioCardWithoutData from '@/src/entities/portfolio/ui/BotPortfolioCard'
 
 export interface BotPortfolioCardProps {
   botUrl: string
@@ -11,25 +10,5 @@ export default function BotPortfolioCard({ botUrl, className = '' }: BotPortfoli
   const { data: currencies } = trpc.control.portfolio.getCurrencies.useQuery({ url: botUrl })
   const { data: securities } = trpc.control.portfolio.getSecurities.useQuery({ url: botUrl })
 
-  return (
-    <div className={`card card-compact bg-base-200 ${className}`}>
-      <div className="card-body">
-        <h3 className="card-title">Portfolio</h3>
-        <h4 className="text-lg font-bold">Currencies</h4>
-        <div className="card bg-base-100">
-          <div className="flex gap-4 p-3">
-            {currencies?.map((item) => (
-              <SecurityBalanceListItem key={item.assetTicker} item={item} />
-            ))}
-          </div>
-        </div>
-        <h4 className="text-lg font-bold">Securities</h4>
-        <ul className="max-h-[50vh] overflow-y-auto">
-          {securities?.map((item) => (
-            <PortfolioListItem key={item.assetTicker} item={item} className="my-1" />
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
+  return <BotPortfolioCardWithoutData currencies={currencies!} securities={securities!} className={className} />
 }
