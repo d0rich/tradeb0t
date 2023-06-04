d47985cde5a1dc88cf4d53338fb6351e6002ce14
Nikolay Dorofeev <dorich2000@gmail.com>
2023-06-04 20:04:22 +0400

    Loading indicator on pages

--- apps/dashboard/src/pages/bots/[botUrl]/algorithms/[algorithmName].tsx
+++ apps/dashboard/src/pages/bots/[botUrl]/algorithms/[algorithmName].tsx
@@ -3,19 +3,36 @@ import { useRouter } from 'next/router'
 import { AlgorithmRunsTable } from '@/src/widgets/AlgorithmRunsTable'
 import BotHeaderDescriptor from '@/src/widgets/bot/BotHeaderDescriptor'
 import AlgorithmHeaderDescriptor from '@/src/widgets/algorithm/AlgorithmHeaderDescriptor'
+import Loading from '@/src/shared/ui/Loading'
 
 export default function AlgorithmRunsPage() {
   const {
     query: { botUrl, algorithmName, page }
   } = useRouter()
 
-  const { data: bot } = trpc.repository.findBot.useQuery({ url: String(botUrl) })
-  const { data: algorithms } = trpc.control.algorithms.list.useQuery({ url: String(botUrl) })
+  const {
+    data: bot,
+    isLoading: isLoadingBot,
+    isError: isErrorBot
+  } = trpc.repository.findBot.useQuery({ url: String(botUrl) })
+  const {
+    data: algorithms,
+    isLoading: isLoadingAlgorithm,
+    isError: isErrorAlgorithm
+  } = trpc.control.algorithms.list.useQuery({ url: String(botUrl) })
 
   const algorithm = algorithms?.find((algorithm) => algorithm.name === algorithmName)
 
-  if (!bot || !algorithm) {
+  if (isErrorAlgorithm || isErrorBot) {
     return <h1 className="font-bold text-3xl m-5 text-error">Bot not found</h1>
+  }
+
+  if (isLoadingAlgorithm || isLoadingBot) {
+    return (
+      <div className="w-full h-[66vh] flex items-center justify-center">
+        <Loading />
+      </div>
+    )
   }
 
   function pageLinkPattern(page: number) {
@@ -24,8 +41,8 @@ export default function AlgorithmRunsPage() {
 
   return (
     <>
-      <BotHeaderDescriptor bot={bot} />
-      <AlgorithmHeaderDescriptor botUrl={bot.url} algorithm={algorithm} />
+      <BotHeaderDescriptor bot={bot!} />
+      <AlgorithmHeaderDescriptor botUrl={botUrl as string} algorithm={algorithm!} />
       <h2 className="text-3xl font-bold my-7">Algorithm runs</h2>
       <AlgorithmRunsTable
         pageLinkPattern={pageLinkPattern}
--- apps/dashboard/src/pages/bots/[botUrl]/orders.tsx
+++ apps/dashboard/src/pages/bots/[botUrl]/orders.tsx
@@ -2,21 +2,30 @@ import { trpc } from '@/src/shared/api/trpc'
 import { useRouter } from 'next/router'
 import BotHeaderDescriptor from '@/src/widgets/bot/BotHeaderDescriptor'
 import OrdersTable from '@/src/widgets/OrdersTable'
+import Loading from '@/src/shared/ui/Loading'
 
 export default function AlgorithmRunsPage() {
   const {
     query: { botUrl }
   } = useRouter()
 
-  const { data: bot } = trpc.repository.findBot.useQuery({ url: String(botUrl) })
+  const { data: bot, isError, isLoading } = trpc.repository.findBot.useQuery({ url: String(botUrl) })
 
-  if (!bot) {
+  if (isError) {
     return <h1 className="font-bold text-3xl m-5 text-error">Bot not found</h1>
+  }
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-[66vh] flex items-center justify-center">
+        <Loading />
+      </div>
+    )
   }
 
   return (
     <>
-      <BotHeaderDescriptor bot={bot} />
+      <BotHeaderDescriptor bot={bot!} />
       <OrdersTable botUrl={String(botUrl)} />
     </>
   )
