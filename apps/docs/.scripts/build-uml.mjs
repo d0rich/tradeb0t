f56a44346bab22899903368651a36b5cf7140ff3
Nikolay Dorofeev <dorich2000@gmail.com>
2023-06-05 14:46:58 +0400

    docs portal as app in monorepo

--- apps/docs/.scripts/build-uml.mjs
+++ apps/docs/.scripts/build-uml.mjs
@@ -12,9 +12,9 @@ function buildUml(srcFiles, diagramName, args = '') {
   console.log(`Generated ${diagramName}.svg`)
 }
 
-buildUml('../packages/core/src/**/*.ts', 'src', '-I')
-buildUml('../packages/core/src/domain/**/*.ts', 'domain')
-buildUml('../packages/core/src/bot/**/*.ts', 'bot')
-buildUml('../packages/core/src/connector/**/*.ts', 'connector')
-buildUml('../packages/core/src/storage/**/*.ts', 'storage')
-buildUml('../packages/core/src/algorithms/**/*.ts', 'algorithms')
+buildUml('../../packages/core/src/**/*.ts', 'src', '-I')
+buildUml('../../packages/core/src/domain/**/*.ts', 'domain')
+buildUml('../../packages/core/src/bot/**/*.ts', 'bot')
+buildUml('../../packages/core/src/connector/**/*.ts', 'connector')
+buildUml('../../packages/core/src/storage/**/*.ts', 'storage')
+buildUml('../../packages/core/src/algorithms/**/*.ts', 'algorithms')
