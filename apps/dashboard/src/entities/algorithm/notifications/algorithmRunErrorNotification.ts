9f50470477af58f1daf39e3bd05ee22c5fa3a72f
Nikolay Dorofeev <dorich2000@gmail.com>
2023-06-05 21:05:10 +0400

    Fix notification code text color

--- apps/dashboard/src/entities/algorithm/notifications/algorithmRunErrorNotification.ts
+++ apps/dashboard/src/entities/algorithm/notifications/algorithmRunErrorNotification.ts
@@ -3,7 +3,7 @@ import type { Algorithm } from '@tradeb0t/core'
 
 export function algorithmRunErrorNotification(algorithm: Algorithm): NotificationToPush {
   return {
-    content: `Failed to run <code class="kbd kbd-sm text-white">${algorithm.name}</code> 😔.`,
+    content: `Failed to run <code class="kbd kbd-sm text-base-content">${algorithm.name}</code> 😔.`,
     type: 'error'
   }
 }
